483ccce0339b52924d6b33eaedef46bca906ec76
Mac Ignacio <mack_ignacio@yahoo.com>
2021-10-31 23:57:50 +0800

    Update db.ts

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+// Why use async when you don't have something to await?
 const connectDB = async () => {
 
     const mongoUriConnString: string = process.env.MONGO_URI
